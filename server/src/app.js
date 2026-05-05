f6e02e7b22b2d43009792672b58063ecf5e97bb6
Project Contributor <contributor@example.com>
2026-05-05 23:25:57 +0530

    fix: use (.*) for catch-all route to support Express 5 / path-to-regexp v6

--- server/src/app.js
+++ server/src/app.js
@@ -34,7 +34,7 @@ app.use('/api/tasks', require('./routes/taskRoutes'));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../../client/dist')));
 
-  app.get('/*', (req, res) =>
+  app.get('(.*)', (req, res) =>
     res.sendFile(path.resolve(__dirname, '../../client', 'dist', 'index.html'))
   );
 } else {
